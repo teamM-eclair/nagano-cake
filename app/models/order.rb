f68200881345cf61429f380a1f5e10153eece6c1
EC2 Default User <ec2-user@ip-172-31-83-195.ec2.internal>
2022-05-19 09:24:19 +0000

    add-enum

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,6 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
 
+  #enumにて支払方法の数値と文字列の紐付け
+  enum payment_method: { credit_card: 0, transfer: 1 }
 end
