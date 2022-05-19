96277f9911bbc2dc9d5218880ee6bb09bf95c412
EC2 Default User <ec2-user@ip-172-31-83-195.ec2.internal>
2022-05-19 05:19:20 +0000

    add_routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,33 +9,36 @@ Rails.application.routes.draw do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :orders, only: [:show, :update]
     resources :order_details, only: [:update]
-
-  get 'deliveries/index'
-  get 'deliveries/edit'
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/thanx'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/quit'
-    get 'customers/edit'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
   
+  namespace :public do
+    root to: "homes#top"
+    get "about" => "homes#about"
+
+    resources :customers, only: [:edit, :update] do
+      get "customers/my_page" => "customers#show"
+      collection do
+        get "quit"
+        patch "out"
+      end
+    end
+    
+    resources :items, only: [:index, :show]
+
+    resources :orders, only: [:new, :create, :index, :show] do
+      collection do
+        post "confirm"
+        get "thanx"
+      end
+    end
+  end
+
+  resources :cart_items, only: [:index, :update, :create, :destroy] do
+    collection do
+      delete "all_delete"
+    end
+  end
+
+  resources :deliveries, only: [:index, :create, :destroy, :edit, :update]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
