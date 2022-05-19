a03dea7b9e3e320f85dada71157e1c6f7b67558b
EC2 Default User <ec2-user@ip-172-31-83-195.ec2.internal>
2022-05-19 01:08:18 +0000

    add-customer-controller

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,16 @@
+class Public::CustomersController < ApplicationController
+  def show
+  end
+
+  def quit
+  end
+  
+  def out
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
+  get 'deliveries/index'
+  get 'deliveries/edit'
+  namespace :public do
+    get 'orders/new'
+    get 'orders/confirm'
+    get 'orders/thanx'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/quit'
+    get 'customers/edit'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
