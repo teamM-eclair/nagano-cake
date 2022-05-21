08db2338f3f212fdc3fe1becd509262a125bdaa4
AkiraKimura <102400100+kapi-barasan@users.noreply.github.com>
2022-05-21 11:39:22 +0000

    add customer sign_in,sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,28 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_customer!, except: [:top, :about]
-  before_action :authenticate_admin!, except: [:top, :about]
-    
+  #before_action :authenticate_customer!, except: [:top, :about]
+  #before_action :authenticate_admin!, except: [:top, :about]
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
 
-  def after_sign_in_path_for(resource)
-    user_path(current_user)
+  private
+  def after_sign_in_path_for(resource_or_scope)
+      if resource_or_scope.is_a?(Admin)
+          admin_root
+      else
+          public_customer_customers_my_page_path(current_customer)
+      end
   end
 
-  def after_sign_out_path_for(resource)
-    root_path
+  def
+  after_sign_out_path_for(resource_or_scope)
+      if resource_or_scope == :customer
+          public_root_path
+      elsif resource_or_scope == :admin
+          new_admin_session_path
+      else
+          root_path
+      end
   end
 
   def configure_permitted_parameters
