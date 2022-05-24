9b88705cc3185dc6b76eea09fa1675e3b83f3301
namamoto <101371743+namamoto@users.noreply.github.com>
2022-05-24 14:49:46 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 2022_05_18_101016) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "amount"
+    t.integer "item_id"
+    t.integer "customer_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
