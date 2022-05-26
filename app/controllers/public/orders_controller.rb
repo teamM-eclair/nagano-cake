d0cc3a3fde81ae4b04d7350af9dcd8f3c6e0a2b6
namamoto <emaraku1009@gmail.com>
2022-05-26 01:18:45 +0000

    adminレイアウト

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,48 +6,47 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    # 支払方法、新しいお届け先の場合の情報は10行目に入っている
     @order = Order.new(order_params)
     @cart_items = current_customer.cart_items
     @customer = current_customer
-    
+
+    # 支払方法---いらない---------------------------------
+    # if params[:order][:payment_method] == "credit_card"
+    #   @payment_method = Order.payment_methods.key(0)
+    # else
+    #   @paymentmethod = Order.payment_methods.key(1)
+    # end
+
     # お届け先
-        # ご自身の住所の場合
     if params[:order][:shipping_address] == "0"
       @order.postcode = @customer.postcode
       @order.address = @customer.address
-      @order.name = @customer.last_name + @customer.first_name
-      # 登録済み住所の場合
+      @order.name = @customer.name
     elsif params[:order][:shipping_address] == "1"
-      @delivery = Delivery.find(params[:order][:address_id])
-      @order.postcode = @delivery.postcode
+      @delivery = Delivery.find([:order][:address_id])
+      @order.postcode = @derivery.postcode
       @order.address = @delivery.address
       @order.name = @delivery.name
     end
+    #新規配送先の場合
 
+
+    # end
+
+    # @order.postcode = current_customer.postcode
+    # @order.address = current_customer.address
+    # @order.name = current_customer.first_name + current_customer.last_name
 
 
   end
 
   def create
     @order = Order.new(order_params)
-    @order.customer_id = current_customer.id
     # Orderモデルに注文を保存
-    @order.save     
-    
-    current_customer.cart_items.each do |cart_item|
-      
-    # @order_detail = OrderDetail.new(order_detail_params) 
-    
-    
-   
     # OrderDetailモデルにカート内商品の情報をもとに保存
-    
-    
-    
-    
+    @order.save
+    .save
     # カート内商品をすべ削除
-    current_customer.cart_items.destroy_all # @order.destroy.all?
     # 購入画面に遷移
     redirect_to thanx_public_orders_path
   end
@@ -67,10 +66,6 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:customer_id, :shipping_fee, :bill, :payment_method, :address, :postcode, :name, :status)
+    params.require(:order).permit(:customer_id, :payment_method, :address, :postcode, :name)
   end
-  
-  # def order_detail_params
-  #   params.require(:order_detail).permit(:payment_method, :address, :postcode, :name)    
-  # end
 end
