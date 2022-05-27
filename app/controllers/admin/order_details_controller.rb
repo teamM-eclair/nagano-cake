0a7340936c3435409898808acbe38445667f3ed2
t-natori <102852756+t-natori@users.noreply.github.com>
2022-05-27 03:42:52 +0000

    微調整

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -12,7 +12,7 @@ class Admin::OrderDetailsController < ApplicationController
 			
 			# カート内商品すべてが製作完了なら
 		elsif @order.order_details.count ==  @order.order_details.where(product_status: "completion_of_production").count
-			#注文ステータスを3(発送準備中)に更新
+		  #注文ステータスを3(発送準備中)に更新
 			@order.update(status: 3) 
 		end
 		#注文詳細に遷移
